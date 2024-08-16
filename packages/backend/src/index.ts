926d42492efe5dc65d0be2bf072628811e30d451
Shane Warner <shane@3dlabs.io>
2024-08-16 14:44:19 -0400

    feat: add module for @internal scope

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -47,4 +47,7 @@ backend.add(import('@backstage/plugin-search-backend-module-pg/alpha'));
 backend.add(import('@backstage/plugin-search-backend-module-catalog/alpha'));
 backend.add(import('@backstage/plugin-search-backend-module-techdocs/alpha'));
 
+// Terraform module
+backend.add(import('@internal/plugin-scaffolder-backend-module-terraform'));
+
 backend.start();
