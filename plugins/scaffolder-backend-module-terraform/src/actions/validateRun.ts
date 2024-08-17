cb1e848d2bec618ee93eb17af733a543fec5ec5a
Shane Warner <shane@3dlabs.io>
2024-08-16 23:56:43 -0400

    feat: add additional handling for various run statuses

--- plugins/scaffolder-backend-module-terraform/src/actions/validateRun.ts
+++ plugins/scaffolder-backend-module-terraform/src/actions/validateRun.ts
@@ -49,21 +49,43 @@ export const createTerraformValidateRunAction = (options: {
       const { runID, token } = ctx.input;
     
       const terraformApi = new TerraformClient(options, token);
-      const pollInterval = 30; // Poll interval
+      const pollSeconds = 30; // Number of seconds to poll for status
+      const pollInterval = pollSeconds * 1000; // Poll interval
       const targetStatus = 'applied'; // Target status to check for
     
       let run;
     
       while (true) {
         run = await terraformApi.getRunStatus(runID);
-    
-        if (run.data.attributes.status === targetStatus) {
+        const runStatus = run.data.attributes.status;
+        
+        if (runStatus === targetStatus) {
           ctx.logger.info(`Run successfully applied: ${run.data.id}`);
           ctx.output('id', run.data.id);
           break; // Exit the loop once the target status is reached
-        }
+        } else if (runStatus === 'errored') {
+          ctx.logger.error("Plan errored or hard-mandatory policy failed.");
+          ctx.output('status', 'errored');
+          break; // Exit on errored status
+        } else if (runStatus === 'planned_and_finished') {
+          ctx.logger.info("Plan indicates no changes to apply.");
+          ctx.output('status', 'planned_and_finished');
+          break; // Exit on planned_and_finished status
+        } else if (runStatus === 'canceled') {
+          ctx.logger.warn("The run was canceled.");
+          ctx.output('status', 'canceled');
+          break; // Exit on canceled status
+        } else if (runStatus === 'force_canceled') {
+          ctx.logger.warn("The run was canceled forcefully.");
+          ctx.output('status', 'force_canceled');
+          break; // Exit on force_canceled status
+        } else if (runStatus === 'discarded') {
+          ctx.logger.warn("The run was discarded.");
+          ctx.output('status', 'discarded');
+          break; // Exit on discarded status
+        }    
     
-        ctx.logger.info(`Run status is ${run.data.attributes.status}. Polling again in 30 seconds...`);
+        ctx.logger.info(`Run status is ${run.data.attributes.status}. Polling again in ${pollSeconds} seconds...`);
     
         // Inline sleep logic
         await new Promise(resolve => setTimeout(resolve, pollInterval));
